445109cd37634758f30e8130df0eb3276532d6d8
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2023-09-06 21:32:15 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/header';
-console.log(Header);
+
 
 function App() {
 
   return (
-    <div className="App">
-
+    <div>
       <Header />
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendiendo React
-        </a>
-      </header>
+      
     </div>
   );
 }
