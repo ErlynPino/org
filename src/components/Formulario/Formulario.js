66cd4bafdeabd7c32a6be46e851132996bc1ff31
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2023-09-08 00:26:46 -0500

    manejo de eventos react

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -4,14 +4,22 @@ import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
 const Formulario = () => {
+
+    const manejarSubmit = (e) => {
+        e.preventDefault()
+        console.log("Manejar el envio", e)
+    }
+
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarSubmit}>
             <h2>Rellena el formulario para crear el colaborador</h2>
             <CampoText titulo="Nombre" placeholder="Ingresar nombre" />
             <CampoText titulo="Puesto" placeholder="Ingresar Puesto"/>
             <CampoText titulo="Foto" placeholder="Ingresar enlace de Foto"/>
             <ListaOpciones />
-            <Boton texto="Crear" />
+            <Boton> 
+                Crear
+            </Boton>
         </form>
     </section>
 }
