853c3d8f504ec86787a59063115f29b5fc1b98ba
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2023-09-09 12:24:49 -0500

    manejo de estados

--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "./MiOrg.css"
 
 const MiOrg = () => {
+    const [mostrar, actualizarMostrar] = useState(true);   
+
+    const manejarClick = () => {
+        console.log("Mostrar/Ocultar elemnto", mostrar);
+        actualizarMostrar(!mostrar)
+    }
+
     return <section className="orgSection">
         <h3 className="title">Mi Organización</h3>
-        <img src="/img/add.png" alt="add" />
+        <img src="/public/logo192.png" alt="add" onClick={manejarClick}/>
     </section>
 }
 
